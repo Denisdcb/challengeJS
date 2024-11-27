6198ebee234c95af61cfbcc344dd0c0cab87815d
Denisdcb <denis.dcbr@gmail.com>
2024-11-27 12:37:03 +0100

    Challenge 3 4 5 JS

--- challenge4/script.js
+++ challenge4/script.js
@@ -1,9 +1,65 @@
-let a = 1, b = 1 ;
-let c = ++a ;
+// Données 1
 
-let d = b++ ;
+let dauphinsScores = [96, 108, 89];
+let koalasScores = [88, 91, 110];
 
-// a vaut 2 
-// b vaut 2
-// c vaut 2
-// d vaut 1
+let winner = '';
+
+moyDauphinsScores = (dauphinsScores[0] + dauphinsScores[1] + dauphinsScores[2]) / 3 ;
+moyKoalasScores = (koalasScores[0] + koalasScores[1] + koalasScores[2]) / 3;
+
+if(moyDauphinsScores === moyKoalasScores) {
+    winner = 'Equality !!';
+} else if(moyDauphinsScores > moyKoalasScores && moyDauphinsScores >= 100) {
+    winner = 'Dauphins Team Win !!';
+} else if(moyKoalasScores > moyDauphinsScores && moyKoalasScores > 100) {
+    winner = 'Koalas Team Win !!';
+} else {
+    winner = 'No winner..'
+}
+
+console.log(winner);
+
+// Données 2
+
+dauphinsScores = [97, 112, 101];
+koalasScores = [109, 95, 123];
+
+winner = '';
+
+moyDauphinsScores = (dauphinsScores[0] + dauphinsScores[1] + dauphinsScores[2]) / 3 ;
+moyKoalasScores = (koalasScores[0] + koalasScores[1] + koalasScores[2]) / 3;
+
+if(moyDauphinsScores === moyKoalasScores) {
+    winner = 'Equality !!';
+} else if(moyDauphinsScores > moyKoalasScores && moyDauphinsScores >= 100) {
+    winner = 'Dauphins Team Win !!';
+} else if(moyKoalasScores > moyDauphinsScores && moyKoalasScores > 100) {
+    winner = 'Koalas Team Win !!';
+} else {
+    winner = 'No winner..'
+}
+
+console.log(winner);
+
+// Données 3
+
+dauphinsScores = [96, 108, 89];
+koalasScores = [88, 91, 106];
+
+winner = '';
+
+moyDauphinsScores = (dauphinsScores[0] + dauphinsScores[1] + dauphinsScores[2]) / 3 ;
+moyKoalasScores = (koalasScores[0] + koalasScores[1] + koalasScores[2]) / 3;
+
+if(moyDauphinsScores === moyKoalasScores) {
+    winner = 'Equality !!';
+} else if(moyDauphinsScores > moyKoalasScores && moyDauphinsScores >= 100) {
+    winner = 'Dauphins Team Win !!';
+} else if(moyKoalasScores > moyDauphinsScores && moyKoalasScores > 100) {
+    winner = 'Koalas Team Win !!';
+} else {
+    winner = 'No winner..'
+}
+
+console.log(winner);

92ef9cd78254b854f2f867cedb85ec33db74ad55
Denisdcb <denis.dcbr@gmail.com>
2024-12-09 11:42:03 +0100

    Challenge 12 JS

---
+++ challenge12/script.js
@@ -0,0 +1,16 @@
+// Données de test 1
+
+let temp = [17, 21, 23];
+console.log('Données 1')
+for(i = 0; i < temp.length; i++) {
+    console.log(`${temp[i]} degré dans ${i+1} jours`)
+}
+
+// Données de test 2
+
+temp = [12, 5, -5, 0, 4];
+
+console.log('Données 2')
+for(i = 0; i < temp.length; i++) {
+    console.log(`${temp[i]} degré dans ${i+1} jours`)
+}
